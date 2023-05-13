17e2c6272a1a651303dfb49e3ab3a9881e18fd90
techcoderlab <c.hassanshahzad@gmail.com>
2023-05-13 18:53:56 +0500

    Instagram Project Live Search

---
+++ Mini-Projects/LiveSearch/script.js
@@ -0,0 +1,60 @@
+const result = document.getElementById("result");
+const filter = document.getElementById("filter");
+const loading = document.querySelector("#result li");
+const listItems = [];
+const filteredItems = [];
+
+getData();
+
+filter.addEventListener("input", (e) => filterData(e.target.value));
+
+async function getData() {
+  try {
+    loading.classList.remove("hide");
+
+    const res = await fetch("https://randomuser.me/api?results=50");
+    const { results } = await res.json();
+
+    loading.classList.add("hide");
+
+    const fragment = document.createDocumentFragment();
+
+    results.forEach((user) => {
+      const li = document.createElement("li");
+      listItems.push(li);
+
+      li.innerHTML = `
+            <img src="${user.picture.large}" alt="${user.name.first}">
+            <div class="user-info">
+                <h4>${user.name.first} ${user.name.last}</h4>
+                <p>Phone: ${user.phone}</p>
+            </div>
+        `;
+
+      fragment.appendChild(li);
+    });
+
+    result.appendChild(fragment);
+  } catch (error) {
+    console.log("Error occurred:", error);
+  }
+}
+
+function filterData(searchTerm) {
+function filterData(searchTerm) {
+  filteredItems.length = 0;
+
+  listItems.forEach((item) => {
+    const name = item.querySelector("h4").textContent.toLowerCase();
+    const phoneNo = item.querySelector("p").textContent.toLowerCase();
+    if (
+      name.includes(searchTerm.toLowerCase()) ||
+      phoneNo.includes(searchTerm.toLowerCase())
+    ) {
+      filteredItems.push(item);
+      item.classList.remove("hide");
+    } else {
+      item.classList.add("hide");
+    }
+  });
+}
