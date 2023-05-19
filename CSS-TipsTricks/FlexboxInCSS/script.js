2d4d19c45336bd93210fafe05f7c1eed23dbc034
techcoderlab <c.hassanshahzad@gmail.com>
2023-05-19 16:36:34 +0500

    Instagram Reel - Flexbox In CSS

---
+++ CSS-TipsTricks/FlexboxInCSS/script.js
@@ -0,0 +1,19 @@
+const radioButtons = document.querySelectorAll('input[type="radio"]');
+const parentDiv = document.getElementById("parent");
+
+radioButtons.forEach(function (radio) {
+  radio.addEventListener("change", function (event) {
+    const selectedRadioButtonName = this.name;
+    const selectedRadioButtonValue = this.value;
+
+    if (selectedRadioButtonName === "flex-direction") {
+      parentDiv.style.flexDirection = selectedRadioButtonValue;
+    } else if (selectedRadioButtonName === "flex-wrap") {
+      parentDiv.style.flexWrap = selectedRadioButtonValue;
+    } else if (selectedRadioButtonName === "justify-content") {
+      parentDiv.style.justifyContent = selectedRadioButtonValue;
+    } else if (selectedRadioButtonName === "align-items") {
+      parentDiv.style.alignItems = selectedRadioButtonValue;
+    }
+  });
+});
